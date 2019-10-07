5e723c685896246dc162a65cad5f05279635e4c5
Paola Villalobos <56058919+PaolaVs@users.noreply.github.com>
2019-10-06 22:50:54 -0600

    Add files via upload

---
+++ EVA1_11_ARREGLOS_MULTIDIMENSIONALES/src/Princiapal.java
@@ -0,0 +1,20 @@
+
+public class Princiapal {
+
+  
+    public static void main(String[] args) {
+      //ARREGLO DE 2 DIMENSIONES-->MATRIZ
+      int aDatos[][] = new int [3][4];
+        System.out.println("Direccion del arreglo " + aDatos);
+        System.out.println("Tamaño del arreglo " + aDatos.length);//direccion del arreglo
+        
+        System.out.println("Direccion del arreglo  aDatos[0]" + aDatos[0]);
+        System.out.println("Tamaño del arreglo aDatos[0] " + aDatos[0].length);//DIRECCION
+        
+        System.out.println("Valor de la posicion[0][0] " + aDatos[0][0]);//DATOS
+        
+        
+    }
+    
+    
+}
