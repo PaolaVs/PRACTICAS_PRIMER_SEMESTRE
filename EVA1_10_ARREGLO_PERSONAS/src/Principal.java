42b982ce7ac17ffb883bcdbcd64af7494586e161
Paola Villalobos <56058919+PaolaVs@users.noreply.github.com>
2019-10-06 22:49:14 -0600

    Add files via upload

---
+++ EVA1_10_ARREGLO_PERSONAS/src/Principal.java
@@ -0,0 +1,21 @@
+
+public class Principal {
+//Crear clase persona, y crear un arreglo
+//Que alacene las 10 personas
+   
+    public static void main(String[] args) {
+        System.out.println("¿Cuantas Personas ");
+      Personas[] apNombre = new Personas[10];
+      Personas[] apApellido = new Personas[10];
+      
+      
+      
+      // TODO code application logic here
+    }
+    
+}
+class Personas{
+    String Nombre;
+    String Apellido;
+    
+}
